9adb11c6d8cfa312a8cb9be87b0814c28b15b8db
oujinlong <616559837@qq.com>
2016-06-06 15:38:25 +0800

    commit

--- redBook/Lib/OUAnimationNavigation/OUNavigationController.h
+++ redBook/Lib/OUAnimationNavigation/OUNavigationController.h
@@ -10,7 +10,14 @@
 #import "OUNavAnimation.h"
 
 @interface OUNavigationController : UINavigationController
-
+/**
+ *  小红书 Nav push
+ *
+ *  @param viewController 目标 VC
+ *  @param imageView      动画ImageView
+ *  @param desRect        imageView在 目标VC中的 rect
+ *  @param delegate       目标VC 可能所需要实现的代理
+ */
 -(void)pushViewController:(UIViewController *)viewController withImageView:(UIImageView*)imageView desRect:(CGRect)desRect delegate:(id <OUNavAnimationDelegate>) delegate;
 
 @end
